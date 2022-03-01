02f7297d742e820b510aa587b17d93c6e3f189ee
Caio Augusto <caioamfr@gmail.com>
2022-02-28 21:42:13 -0300

    feat: created object with regex and message for validator

---
+++ src/constants/fields.ts
@@ -0,0 +1,22 @@
+export const fields = {
+  ensureTwoUppercase: {
+    regex: /(?=.*[A-Z])/,
+    message: "Password contain at least one uppercase letter",
+  },
+  ensureOneSpecialChar: {
+    regex: /(?=.*[!@#$&*])/,
+    message: "Password contain at least one special character",
+  },
+  ensureTwoDigits: {
+    regex: /(?=.*[0-9].*[0-9])/,
+    message: "Password contain at least two digits",
+  },
+  ensureThreeLowerCase: {
+    regex: /(?=.*[a-z].*[a-z].*[a-z])/,
+    message: "Password contain at least 3 lowercase letters",
+  },
+  ensureMinLength: {
+    regex: /(.{8})/,
+    message: "Password has a minimum length of 8 characters",
+  },
+};
